7d40eb8b26c890dfc3c9f142ad054c716b64d44e
Alexander P <pcrunn@pm.me>
2020-05-20 19:03:35 +0300

    beautify html

--- index.php
+++ index.php
@@ -2,21 +2,15 @@
 <html>
 
 <head>
-
 	<title>tea</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
 </head>
 
 <body>
-
 	<h1>tea</h1>
-
 	<form action="put.php" method="POST" enctype="multipart/form-data">
 		<input type="file" name="content" onchange="form.submit()">
 	</form>
-
 </body>
 
 </html>
